964b646ffda5c02e9fd39f9d06d7f550cbabfae5
Arnaud PRAT <arnaud.prat@morning.com>
2020-10-30 20:53:53 +0100

    ADD aws amplify

---
+++ src/config.ts
@@ -0,0 +1,17 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default {
+  s3: {
+    REGION: 'eu-west-3',
+    BUCKET: 'rndprt-notes',
+  },
+  apiGateway: {
+    REGION: 'Yeu-west-2',
+    URL: 'https://93onijaqel.execute-api.eu-west-2.amazonaws.com/prod',
+  },
+  cognito: {
+    REGION: 'eu-west-2',
+    USER_POOL_ID: 'eu-west-2_QAdC5syRn',
+    APP_CLIENT_ID: '1ktro707el467kuo2lttg9ssm9',
+    IDENTITY_POOL_ID: 'eu-west-2:9701023a-b7bd-4bc2-a3a9-32553e063670',
+  },
+};
